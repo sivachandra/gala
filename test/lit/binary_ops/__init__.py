80072703d13297da5aaaef26bc1a86af1543c4d5
Jorge Gorbe Moya <jgorbe@google.com>
2021-10-14 16:40:46 -0700

    Add test for addition/subtraction of gdb.Values.
    It doesn't pass yet. We need some changes to deal correctly with
    floats and doubles.

---
+++ test/lit/binary_ops/__init__.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+
+import gdb
+
+# INTEGER OPS
+i1 = gdb.parse_and_eval("i1")
+i2 = gdb.parse_and_eval("i2")
+
+# Sum
+# gdb.Value + gdb.Value
+print("i1 + i2 =", str(i1 + i2))
+# gdbValue + int, and vice versa
+print("i1 + 1 =", str(i1 + 1))
+print("1 + i2 =", str(1 + i2))
+
+# Subtraction
+# gdb.Value - gdb.Value
+print("i1 - i2 =", str(i1 - i2))
+# gdbValue - int, and vice versa
+print("i1 - 1 =", str(i1 - 1))
+print("1 - i2 =", str(1 - i2))
+
+# FLOAT OPS
+# gdb.Values can be created from both C++ float and double values. However,
+# python only has one 'float' type and AFAICT we can only create lldb SBValues
+# from double data (SBData.SetDataFromDoubleArray), so "float + float"
+# operations will actually create a double result.
+f1 = gdb.parse_and_eval("f1")
+f2 = gdb.parse_and_eval("f2")
+d1 = gdb.parse_and_eval("d1")
+d2 = gdb.parse_and_eval("d2")
+
+# Sum
+# float gdb.Value + float gdb.Value
+print("f1 + f2 =", str(f1 + f2))
+# float gdb.Value + float, and vice versa
+print("f1 + 1.0 =", str(f1 + 1.0))
+print("1.0 + f2 =", str(1.0 + f2))
+# double gdb.Value + double gdb.Value
+print("d1 + d2 =", str(d1 + d2))
+# double gdb.Value + float, and vice versa
+print("d1 + 1.0 =", str(d1 + 1.0))
+print("1.0 + d2 =", str(1.0 + d2))
+# float gdb.Value + double gdb.Value, and vice versa
+print("f1 + d2 =", str(f1 + d2))
+print("d1 + f2 =", str(d1 + f2))
+
+# Subtraction
+# float gdb.Value - float gdb.Value
+print("f1 - f2 =", str(f1 - f2))
+# float gdb.Value - float, and vice versa
+print("f1 - 1.0 =", str(f1 - 1.0))
+print("1.0 - f2 =", str(1.0 - f2))
+# double gdb.Value - double gdb.Value
+print("d1 - d2 =", str(d1 - d2))
+# double gdb.Value - float, and vice versa
+print("d1 - 1.0 =", str(d1 - 1.0))
+print("1.0 - d2 =", str(1.0 - d2))
+# float gdb.Value - double gdb.Value, and vice versa
+print("f1 - d2 =", str(f1 - d2))
+print("d1 - f2 =", str(d1 - f2))
+
---
+++ test/lit/binary_ops/binary_ops.cc
@@ -0,0 +1,10 @@
+int i1 = 3;
+int i2 = 2;
+
+float f1 = 3.25f;
+float f2 = 2.25f;
+
+double d1 = 3.5;
+double d2 = 2.5;
+
+int main() { return 0; }
