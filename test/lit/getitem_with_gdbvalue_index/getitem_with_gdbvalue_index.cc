sivachandra/gala

commit b2fa6936433f19b636602b9bed48fe591f1402f0

Allow `value["member"]` when value is an array of structs.
Also fix the `simple` test case. After commit 364867e printing a char*
no longer prints the pointed-to string, so we call `value.string()` in
the test script.

gdb/__init__.py
@@ -555,15 +555,22 @@ class Value(object):
 
     def __getitem__(self, index):
         sbtype = self._sbvalue_object.GetType()
-        stripped_sbtype, type_class = self._stripped_sbtype()
-        # If ptr["member name"], we need to dereference the pointer.
-        if type_class == lldb.eTypeClassPointer and isinstance(index, str):
-            val = Value(
-                self._sbvalue_object.Cast(stripped_sbtype).Dereference())
-            stripped_sbtype, type_class = val._stripped_sbtype()
-            stripped_sbval = val.sbvalue().Cast(stripped_sbtype)
+        # Keep the value to print in `val`. It's not always `self`:
+        # - If ptr["member name"], we need to dereference the pointer.
+        # - If array["member name"], decay to pointer and dereference.
+        val = self
+        if (sbtype.GetTypeClass() == lldb.eTypeClassPointer and
+            isinstance(index, str)):
+            val = Value(self._sbvalue_object.Dereference())
+        elif (sbtype.GetTypeClass() == lldb.eTypeClassArray and
+              isinstance(index, str)):
+            val = Value(self._sbvalue_object.GetChildAtIndex(0))
         else:
-            stripped_sbval = self._sbvalue_object.Cast(stripped_sbtype)
+            val = self
+
+        stripped_sbtype, type_class = val._stripped_sbtype()
+        stripped_sbval = val.sbvalue().Cast(stripped_sbtype)
+
         if (type_class == lldb.eTypeClassClass or
             type_class == lldb.eTypeClassStruct or
             type_class == lldb.eTypeClassUnion):

test/lit/getitem_with_gdbvalue_index.test
@@ -1,4 +1,4 @@
-; Check that you can pass a numeric gdb.Value to __getitem__.
+; Check that __getitem__ works correctly with different index types.
 
 ; RUN: %clangxx -g -o %t getitem_with_gdbvalue_index/getitem_with_gdbvalue_index.cc
 ; RUN: %lldb -b -o 'b main' -o 'r' -o 'script import getitem_with_gdbvalue_index' %t | FileCheck %s
@@ -16,6 +16,7 @@
 
 ; CHECK: ptr_to_array[Value(int)] -> 22
 ; CHECK: ptr_to_struct[str] -> 99
+; CHECK: struct_array[str] -> 1234
 
 ; CHECK: value(int)[int] -> gdb.error
 ; CHECK: value(int)[value(int)] -> gdb.error

test/lit/getitem_with_gdbvalue_index/__init__.py
@@ -40,6 +40,10 @@ print("ptr_to_array[Value(int)] -> %d" % ptr_to_array[int_index])
 ptr_to_struct = gdb.parse_and_eval("ptr_to_struct")
 print("ptr_to_struct[str] -> %d" % ptr_to_struct["my_value"])
 
+# value["member_name"] on array of structs
+struct_array = gdb.parse_and_eval("struct_array")
+print("struct_array[str] -> %d" % struct_array["my_value"])
+
 # getitem on incorrect types
 one = gdb.parse_and_eval("1")
 member = gdb.parse_and_eval('"member"')

test/lit/getitem_with_gdbvalue_index/getitem_with_gdbvalue_index.cc
@@ -23,5 +23,6 @@ struct MyStruct {
 
 MyStruct my_struct = {99};
 MyStruct *ptr_to_struct = &my_struct;
+MyStruct struct_array[2]= {{1234}, {5678}};
 
 int main() { return 0; }

test/lit/simple/__init__.py
@@ -4,4 +4,4 @@ print(gdb.parse_and_eval("i"))
 print(gdb.parse_and_eval("f"))
 print(gdb.parse_and_eval("d"))
 print(gdb.parse_and_eval("c"))
-print(gdb.parse_and_eval("s"))
+print(gdb.parse_and_eval("s").string())

commit ae709c9390836da71828180fe27f4d8879b03cde

Make pointer_to_struct["member"] see through typedefs.
This was accidentally regressed by commit b2fa693. Now we have tests.

gdb/__init__.py
@@ -559,11 +559,11 @@ class Value(object):
         # - If ptr["member name"], we need to dereference the pointer.
         # - If array["member name"], decay to pointer and dereference.
         val = self
-        if (sbtype.GetTypeClass() == lldb.eTypeClassPointer and
-            isinstance(index, str)):
+        if (sbtype.GetCanonicalType().GetTypeClass() == lldb.eTypeClassPointer
+            and isinstance(index, str)):
             val = Value(self._sbvalue_object.Dereference())
-        elif (sbtype.GetTypeClass() == lldb.eTypeClassArray and
-              isinstance(index, str)):
+        elif (sbtype.GetCanonicalType().GetTypeClass() == lldb.eTypeClassArray
+              and isinstance(index, str)):
             val = Value(self._sbvalue_object.GetChildAtIndex(0))
         else:
             val = self

test/lit/getitem_with_gdbvalue_index.test
@@ -16,7 +16,9 @@
 
 ; CHECK: ptr_to_array[Value(int)] -> 22
 ; CHECK: ptr_to_struct[str] -> 99
+; CHECK: typedefed_ptr_to_struct[str] -> 42
 ; CHECK: struct_array[str] -> 1234
+; CHECK: typedefed_struct_array[str] -> 1234
 
 ; CHECK: value(int)[int] -> gdb.error
 ; CHECK: value(int)[value(int)] -> gdb.error

test/lit/getitem_with_gdbvalue_index/__init__.py
@@ -40,9 +40,15 @@ print("ptr_to_array[Value(int)] -> %d" % ptr_to_array[int_index])
 ptr_to_struct = gdb.parse_and_eval("ptr_to_struct")
 print("ptr_to_struct[str] -> %d" % ptr_to_struct["my_value"])
 
+typedefed_ptr_to_struct = gdb.parse_and_eval("typedefed_ptr_to_struct")
+print("typedefed_ptr_to_struct[str] -> %d" % typedefed_ptr_to_struct["my_value"])
+
 # value["member_name"] on array of structs
 struct_array = gdb.parse_and_eval("struct_array")
 print("struct_array[str] -> %d" % struct_array["my_value"])
+
+typedefed_struct_array = gdb.parse_and_eval("typedefed_struct_array")
+print("typedefed_struct_array[str] -> %d" % typedefed_struct_array["my_value"])
 
 # getitem on incorrect types
 one = gdb.parse_and_eval("1")

test/lit/getitem_with_gdbvalue_index/getitem_with_gdbvalue_index.cc
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 
 int array[6] = {11, 22, 33, 44, 55, 66};
 
@@ -23,6 +24,15 @@ struct MyStruct {
 
 MyStruct my_struct = {99};
 MyStruct *ptr_to_struct = &my_struct;
+
+// Pointer to struct, but typedef'd.
+std::vector<MyStruct> v = {{42}};
+std::vector<MyStruct>::pointer typedefed_ptr_to_struct = &v[0];
+
+// Array of structs, to check the array["struct_member"] case.
 MyStruct struct_array[2]= {{1234}, {5678}};
+// Same but typedefed.
+typedef MyStruct StructArray[2];
+StructArray typedefed_struct_array = {{1234}, {5678}};
 
 int main() { return 0; }