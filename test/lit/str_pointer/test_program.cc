0bc60dc8451dcb5076abfe24d6dd7cfb26206c2c
Jorge Gorbe Moya <jgorbe@google.com>
2023-04-11 11:39:35 -0700

    Fix warning in test program

--- test/lit/str_pointer/test_program.cc
+++ test/lit/str_pointer/test_program.cc
@@ -3,7 +3,7 @@
 int i = 1234;
 int *int_pointer = &i;
 
-char *char_pointer = "literal string";
+const char *char_pointer = "literal string";
 
 std::string s("std::string");
 std::string *string_pointer = &s;
