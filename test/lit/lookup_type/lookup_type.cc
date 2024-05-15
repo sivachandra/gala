6c2881d5dba2744e8aaaeae3f3c70cfb0dc222b8
Pavel Labath <labath@google.com>
2024-05-15 08:12:03 +0000

    Use SBType::FindFirstType for gdb.lookup_type
    .. instead of `FindTypes(basename)` + custom filtering.
    
    This makes a huge difference when it comes to performance, because
    something like `FindTypes("iterator")` forces lldb to return all
    iterator types anywhere in the program. FindFirstType bails out the
    search as soon as it finds the first match (which is all that we're
    interested in).
    
    It's unclear why the function was implemented as it was, but a part of
    the reason was likely the fact that FindTypes had problems with looking
    up/parsing compex type names containing templates. This has now been
    (mostly) fixed in llvm/llvm-project#91586, and appears that the
    simplified version performs satisfactorily.

--- test/lit/lookup_type/lookup_type.cc
+++ test/lit/lookup_type/lookup_type.cc
@@ -1,3 +1,20 @@
-class MyClass{};
+class MyClass {};
 MyClass x;
+
+namespace ns {
+class ClassInNS {};
+ClassInNS class_in_ns;
+}  // namespace ns
+
+template <typename T>
+struct Templated {
+  struct InTemplate {};
+  template <typename U>
+  struct MoreTemplates {};
+};
+
+Templated<int>::InTemplate in_template;
+Templated<ns::ClassInNS>::InTemplate in_complex_template;
+Templated<ns::ClassInNS>::MoreTemplates<ns::ClassInNS> more_templates;
+
 int main() { return 0; }
