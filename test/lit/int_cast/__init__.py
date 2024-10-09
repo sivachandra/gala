93d0c7c20ee341fb26da3d87ea02840d1a4c817b
Caroline Tice <cmtice@google.com>
2024-10-09 15:09:07 -0700

    Update int_cast test.
    Recently int_cast test has started failing with a complaint that 'numbers'
    is ambiguous (either local variable or std::numbers). This CL changes the
    name of the local variable to eliminate the ambiguity.

--- test/lit/int_cast/__init__.py
+++ test/lit/int_cast/__init__.py
@@ -1,6 +1,6 @@
 import gdb
 
-print("b1: %d" % gdb.parse_and_eval("numbers.b1"))
-print("(int)b1: %d" % gdb.parse_and_eval("numbers.b1").cast(gdb.lookup_type("int")))
-print("s1: %d" % gdb.parse_and_eval("numbers.s1"))
-print("(int)s1: %d" % gdb.parse_and_eval("numbers.s1").cast(gdb.lookup_type("int")))
+print("b1: %d" % gdb.parse_and_eval("my_numbers.b1"))
+print("(int)b1: %d" % gdb.parse_and_eval("my_numbers.b1").cast(gdb.lookup_type("int")))
+print("s1: %d" % gdb.parse_and_eval("my_numbers.s1"))
+print("(int)s1: %d" % gdb.parse_and_eval("my_numbers.s1").cast(gdb.lookup_type("int")))
